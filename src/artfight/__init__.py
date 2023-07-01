d520968af2aab2835049e6c9e04a49033b908f72
NimajnebEC <nimajnebec@users.noreply.github.com>
2023-07-01 22:23:47 +0100

    chore: add more objects to __init__

--- src/artfight/__init__.py
+++ src/artfight/__init__.py
@@ -1,7 +1,19 @@
 """A module for retrieving data from [artfight](https://artfight.net/)."""
 
-__all__ = ("ArtfightClient",)
+__all__ = (
+    "ArtfightClient",
+    "Attack",
+    "PartialAttack",
+    "PartialUser",
+    "User",
+    "ArtfightError",
+    "HTTPError",
+    "LoginError",
+    "ParseError",
+)
 __repo__ = r"https://github.com/NimajnebEC/artfight-api"
 __version__ = "0.1.0"
 
 from artfight.client import ArtfightClient
+from artfight.error import ArtfightError, HTTPError, LoginError, ParseError
+from artfight.object import Attack, PartialAttack, PartialUser, User
