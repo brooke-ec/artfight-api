55cbf467417e5e81439946a8a4804f1d823349dd
NimajnebEC <46959407+nimajnebec@users.noreply.github.com>
2024-06-30 22:14:10 +0100

    chore: bump version

--- src/artfight/__init__.py
+++ src/artfight/__init__.py
@@ -12,7 +12,7 @@ __all__ = (
     "ParseError",
 )
 __repo__ = r"https://github.com/NimajnebEC/artfight-api"
-__version__ = "0.1.0"
+__version__ = "0.1.2"
 
 from artfight.client import ArtfightClient
 from artfight.error import ArtfightError, HTTPError, LoginError, ParseError
